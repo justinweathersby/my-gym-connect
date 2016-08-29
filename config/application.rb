10476b1f4b7c5941df6d613c0f8bec3d4513eb58
justinweathersby <justinweathersby@gmail.com>
2016-08-29 10:52:35 -0400

    Setup file to hold and load local env variables

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,13 @@ module MyGymConnect
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Checks for a local_env file in config to set local enviroment variables
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
   end
 end
