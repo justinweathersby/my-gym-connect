c5fd76cc1d2f21c20154341b723529407d791cf0
justinweathersby <justinweathersby@gmail.com>
2016-08-31 09:51:05 -0400

    Added gender to users model

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -4,10 +4,9 @@ class MatchesController < ApplicationController
   def show
     @matches = []
     # @array_of_users = []
-    @in_your_gym = User.where("id != ?", current_user.id).where(:gym_id => current_user.gym_id)
+    @in_your_gym = User.where(:gym_id => current_user.gym_id).where("id != ?", current_user.id).where(:gender => current_user.gender)
 
     @in_your_gym.each do |member|
-      # @array_of_users.push(member)
       intersection = member.hours_in_gym & current_user.hours_in_gym
       if intersection.present?
         @matches.push(member)
