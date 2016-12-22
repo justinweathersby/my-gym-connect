ed275fb1febf1657c5b894fc25854274c1cba0c1
justinweathersby <justinweathersby@gmail.com>
2016-12-22 13:01:12 -0500

    fixed algorithm to deal with gender matches

--- app/controllers/api/v1/matches_controller.rb
+++ app/controllers/api/v1/matches_controller.rb
@@ -3,14 +3,14 @@ class Api::V1::MatchesController < Api::ApiController
 
   def show
     @matches = []
-    
-    if (current_user.workout_time.present? && current_user.gender.present?)
+
+    if (current_user.workout_time.present? && current_user.gender.present? && current_user.gender_match.present?)
       @in_your_gym = User.where(:gym_id => current_user.gym_id).where("id != ?", current_user.id).where.not(:workout_time => nil)
 
       if (current_user.gender_match == 'both')
-        @gender_match = @in_your_gym
+        @gender_match = @in_your_gym.where(:gender_match => 'both')
       else
-        @gender_match = @in_your_gym.where(gender: [current_user.gender_match, 'both'])
+        @gender_match = @in_your_gym.where(:gender => current_user.gender_match).where(:gender_match => current_user.gender)
       end
 
       # .where(:gender => current_user.gender)
