49900587788a751ae7b4763c6dde1cbcf7fe0d31
justinweathersby <justinweathersby@gmail.com>
2017-01-08 12:46:27 -0500

    Added new landing page

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'coffee-rails', '~> 4.1.0'
 # gem 'therubyracer', platforms: :ruby
 
 gem 'bootstrap-sass', '3.3.1'
+gem "font-awesome-rails"
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,8 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += [ 'vendor/simple-line-icons/css/simple-line-icons.css',
+                                                'vendor/device-mockups/device-mockups.min.css',
+                                                'vendor/new-age.css',
+                                                'vendor/bootstrap/css/bootstrap.min.css',
+                                                'vendor/font-awesome/css/font-awesome.min.css']
